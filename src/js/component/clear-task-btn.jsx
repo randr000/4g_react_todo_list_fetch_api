f40772547d4e5c52809d8d175f0c009b302caaab
Raul Andrial <rauljandrial@gmail.com>
2022-11-06 09:28:29 +0000

    Clear Tasks functionality complete

--- src/js/component/clear-task-btn.jsx
+++ src/js/component/clear-task-btn.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from './AppContext.jsx';
 
 const ClearTaskButton = () => {
+
+    const {dispatch} = useContext(AppContext);
+
     return (
-        <button className="btn btn-secondary">Clear Tasks</button>
+        <button className="btn btn-secondary" onClick={e => dispatch({type: 'noTodos'})}>Clear Tasks</button>
     );
 };
 
